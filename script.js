b619c4366cb43caa2b84493f758f5645249c446f
Muhamad Nur Syami <nursami385@gmail.com>
2023-09-27 19:20:00 +0700

    second commit

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 // Function ngambil Nilai Inputan
-function count() {
+function getHandleData() {
   // mengambil value dari inputan KG and CM
   const weight = document.getElementById("weight").value;
   const height = document.getElementById("height").value;
+  return { weight, height };
+}
+
+function count() {
   const total = document.getElementById("total");
   const categories = document.getElementById("categories");
   // mengubah before display none tobe block
   document.getElementById("result").style.display = "block";
   // Perhitungan
-  const sumHeight = weight / (height / 100) ** 2;
+  const sumHeight =
+    getHandleData().weight / (getHandleData().height / 100) ** 2;
   // mengambil 1 angka di belakangan coma (,).
   // menggunakan fungsi toFixed();
   const sum = sumHeight.toFixed(1);
@@ -29,9 +34,29 @@ function count() {
   }
 }
 
+function validateData() {
+  if (getHandleData().weight === "" && getHandleData().height === "") {
+    return false;
+  } else if (getHandleData().weight === "") {
+    return false;
+  } else if (getHandleData().height === "") {
+    return false;
+  } else {
+    return true;
+  }
+}
+
 // Ketika tombol di klik
 const submit = document.getElementById("submit-form");
+const warning = document.getElementById("warning");
+const data = getHandleData();
 submit.addEventListener("click", function (event) {
   event.preventDefault();
-  count();
+  if (validateData() == false) {
+    document.getElementById("result").style.display = "none";
+    warning.innerHTML = "Periksa form anda sekali lagi";
+  } else {
+    warning.innerHTML = "";
+    count();
+  }
 });
